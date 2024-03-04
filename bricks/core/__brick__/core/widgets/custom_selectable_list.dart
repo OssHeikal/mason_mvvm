fdd93bf2aa8c7ae57f0b501b188ffd3e2a890f21
Osama Heikal <109473076+OssHeikal@users.noreply.github.com>
2024-03-04 18:58:03 +0200

    update_brick(core): add custom_selectable_list.dart

---
+++ bricks/core/__brick__/core/widgets/custom_selectable_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../config/extensions/all_extensions.dart';
+import '../resources/resources.dart';
+import 'horizontal_list_view.dart';
+
+class CustomSelectableList extends StatefulWidget {
+  const CustomSelectableList({
+    super.key,
+    required this.items,
+    required this.title,
+    required this.onSelected,
+  });
+
+  final List<String> items;
+  final String title;
+  final ValueChanged<String> onSelected;
+
+  @override
+  State<CustomSelectableList> createState() => _CustomSelectableListState();
+}
+
+class _CustomSelectableListState extends State<CustomSelectableList> {
+  late ValueNotifier<int?> selectedIndex;
+
+  @override
+  void initState() {
+    selectedIndex = ValueNotifier(null);
+    super.initState();
+  }
+
+  void _onSelected(int index) {
+    if (selectedIndex.value == index) {
+      selectedIndex.value = null;
+      widget.onSelected('');
+    } else {
+      selectedIndex.value = index;
+      widget.onSelected(widget.items[index]);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<int?>(
+      valueListenable: selectedIndex,
+      builder: (_, index, __) {
+        return HorizontalListView(
+          height: AppSize.s60.h,
+          title: widget.title,
+          itemCount: widget.items.length,
+          itemBuilder: (_, index) {
+            final textColor = index == selectedIndex.value ? context.scaffoldBackgroundColor : null;
+            final bgColor = index == selectedIndex.value ? context.primaryColor : context.primaryCardColor;
+            return Text(
+              widget.items[index],
+              style: context.titleLarge!.regular.s14.copyWith(color: textColor),
+            )
+                .center()
+                .paddingSymmetric(AppSize.s20, AppSize.s8)
+                .setContainerToView(radius: 100, color: bgColor)
+                .onTap(() => _onSelected(index), borderRadius: 100.0.borderRadius);
+          },
+        );
+      },
+    );
+  }
+}
